0b6e56a2e27dc742ca6ee5d91f2f56cfac89356e
Louis Rabeno <lrabeno@gmail.com>
2022-09-22 16:33:56 -0400

    db and route working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const { Post, User } = require("./db");
 
 app.use(morgan("dev"));
 // app.use(express.static(__dirname + "/public"));
@@ -9,8 +10,29 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("HELLLOOOOO BUDDDY");
+app.get("/", async (req, res, next) => {
+  const posts = await Post.findAll({
+    include: [User],
+  });
+
+  res.send(
+    `
+       <body>
+          ${posts
+            .map(
+              (post) =>
+                `<div>
+                  <h2>
+                    ${post.title}
+                  </h2>
+                  <h3>${post.user.name}</h3>
+                  <p>${post.title}</p>
+                </div>`
+            )
+            .join("")}
+        </body>
+  `
+  );
 });
 // app.get("/", (req, res) => {
 //   res.redirect("/posts");
